a7703a76d403e44dedbbf732d673e6fd26cec823
Jonathan Jensen <haiyun211@gmail.com>
2024-04-30 21:38:12 -0600

    Initial commit

---
+++ src/components/AboutPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AboutPage() {
+    return(
+        <React.Fragment>
+            <h2>About</h2>
+            <p>This app uses react.</p>
+        </React.Fragment>
+    )
+}
+
+export default AboutPage;
---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HomePage() {
+    return (
+        <div className="p-5 mb-4 bg-body-tertiary rounded-3">
+            <h1>Pluralsight Administration</h1>
+            <p>React, Flux, and React Router for ultra-responsive web apps.</p>
+            <a href="/about">About</a>
+        </div>
+    )
+}
+
+export default HomePage;
