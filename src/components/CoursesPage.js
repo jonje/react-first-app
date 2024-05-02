97367a2ce8f02688bce33b2ad5001d4fdcfc318d
Jonathan Jensen <jpjensen@utah.gov>
2024-05-02 16:46:54 -0600

    Add courses page

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getCourses } from "../api/courseApi"
+import CourseList from "./CourseList";
+function CoursesPage() {
+    const [ courses, setCourses ] = useState([]);
 
-class CoursesPage extends React.Component {
-    render() {
-        return <h2>Courses</h2>
-    }
+    useEffect(() => {
+        getCourses().then(_courses => setCourses(_courses));
+    }, []);
+
+    return (
+        <>
+            <h2>Courses</h2>
+            <CourseList courses={courses}/>
+        </>
+    )
 }
 
 export default CoursesPage;
