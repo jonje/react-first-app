fa02585b674e348ad6d9a7caaada0be39744832d
Jonathan Jensen <haiyun211@gmail.com>
2024-05-01 21:35:49 -0600

    Add header and default courses page

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
+import Header from "./common/Header";
 
 function App() {
-    const route = window.location.pathname;
-    if (route === "/about") {
-        return <AboutPage/>
-    } else {
-        return <HomePage/>
+    function getPage() {
+        const route = window.location.pathname;
+        if (route === "/about") {
+            return <AboutPage/>
+        } else {
+            return <HomePage/>
+        }
     }
+    return (
+        <div className="container-fluid">
+            <Header/>
+            { getPage() }
+        </div>
+    )
 }
 
 export default App;
